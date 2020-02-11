e657182003490624d55854062de6a8b8df8bde25
LogicX1 <uninajone@gmail.com>
2020-02-11 19:55:13 +0200

    Users list now also works , for random guest users for now

--- public/script.js
+++ public/script.js
@@ -1,23 +1,42 @@
+
+
 function getId(element) {
-    return document.getElementById(element)
+  return document.getElementById(element);
 }
+function getClass(element) {
+  return document.getElementsByClassName(element);
+}
+function removeChildren(node){
+    while(node.firstChild){
+        node.removeChild(node.firstChild)
+    }
+}
+function updateList(list,values){
+    removeChildren(list);
+    values.forEach(value => {
+        var newListing = document.createElement("li");
+        newListing.textContent = value;
+        list.append(newListing);
+      });
+}
+document.addEventListener("DOMContentLoaded", function() {
+  let socket = io();
 
-document.addEventListener("DOMContentLoaded",function()  {
-    let socket = io();
-    
-    getId('message-form').addEventListener('submit', (e) => {
-        e.preventDefault();
-        console.log(getId('message').value);
-        socket.emit('chat message', getId('message').value)
-        getId('message').value = '';
-        return false;
-    });
+  getId("message-form").addEventListener("submit", e => {
+    e.preventDefault();
+    console.log(getId("message").value);
+    socket.emit("chat message", getId("message").value);
+    getId("message").value = "";
+    return false;
+  });
 
-    socket.on('chat message',function (msg) {
-        console.log('chat message emitted!')
-        var newMsg = document.createElement('li');
-        newMsg.textContent=msg;
-        console.log('message event');
-        getId('messages-list').append(newMsg);
-    });
+  socket.on("chat message", function(msg) {
+    var newMsg = document.createElement("li");
+    newMsg.textContent = msg;
+    getId("messages-list").append(newMsg);
+  });
+
+  socket.on("update connected users", function(connectedUsers) {
+    updateList(getId("users-list"),connectedUsers);
+  });
 });
