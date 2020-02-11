236cd91ba836175017d66a700a0861524a03b6c4
Yousef <yousefrizik92@gmail.com>
2020-02-11 18:28:39 +0200

    adds pg-parser module, adds a new route, login page now works

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const dbConnection = require('../database/db_connection');
 
 const router = express.Router();
 
@@ -8,6 +9,21 @@ router.get('/', (req, res) => {
         layout: 'login'
     })
 })
+
+router.post('/auth', (req, res) => {
+    console.log(req.body)
+    dbConnection.query('SELECT * FROM usernames WHERE $1 = name and $2 = password', [req.body.inputUserName, req.body.inputPassword], (err, result) => {
+        if (result.rows.length) {
+            console.log('work')
+            res.redirect('game')
+        } else {
+            res.render('home', {
+                layout: 'login'
+            })
+        }
+    })
+})
+
 
 router.get('/game', (req, res) => {
     res.render('game', {
